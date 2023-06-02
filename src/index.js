c7461b76dac12606ff44445d9e43958abbfd58d3
thiensinh127 <sinh.tran@terralogic.com>
2023-06-02 22:43:51 +0700

    add boostrap 5

--- src/index.js
+++ src/index.js
@@ -4,16 +4,25 @@ const path = require('path');
 const {engine} = require('express-handlebars');
 const app = express()
 const port = 3000
+
+app.use(express.static(path.join(__dirname, 'public')))
+
 //  HTTP logger
 app.use(morgan('combined'))
 
 //  Template engine
-app.engine('handlebars', engine());
-app.set('view engine', 'handlebars');
-app.set('view', path.join(__dirname, "resources/views"));
+app.engine('hbs', engine({
+  extname: '.hbs',
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, "resources/views"));
 
 app.get('/', (req, res) => {
   res.render('home');
+})
+
+app.get('/news', (req, res) => {
+  res.render('news');
 })
 
 app.listen(port, () => {
